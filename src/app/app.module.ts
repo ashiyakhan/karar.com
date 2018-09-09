2b7484ba75e896da99fcb269cb9a00e6434da455
Ashiya Khan <ashiyak.ak91@gmail.com>
2018-09-09 11:55:33 +0530

    project upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    SidebarComponent,
+    DashboardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
